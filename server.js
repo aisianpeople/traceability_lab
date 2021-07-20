70dec9f6da025f4607bce59003eba75e2c2848c3
Scott Mottola <sjmottola@gmail.com>
2021-07-20 11:24:21 -0600

    update server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const Rollbar = require('rollbar');
 
 const rollbar = new Rollbar({
-  accessTojen: "",
+  accessTojen: '25609357d325482db6e3e4ae2a61e51b',
   captureUncaught: true,
   captureUnhandledRejections: true
 })

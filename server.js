e33369d22d8c3a3757724dcf234def08ce3ec997
Scott Mottola <sjmottola@gmail.com>
2021-07-20 11:30:17 -0600

    updated to heros

--- server.js
+++ server.js
@@ -12,11 +12,37 @@ const rollbar = new Rollbar({
 const app = express();
 app.use(express.json());
 
+let heroList = [];
+
 rollbar.info("Hello World!");
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
     rollbar.info('html file served successfully');
+  });
+
+  app.post("/api/hero", (req, res) => {
+    let { name } = req.body;
+    name = name.trim();
+  
+    const index = heroList.findIndex((heroName) => {
+      return heroName === name;
+    });
+  
+    if (index === -1 && name !== "") {
+      heroList.push(name);
+      // add rollbar log here
+  
+      res.status(200).send(heroList);
+    } else if (name === "") {
+      // add a rollbar error here
+  
+      res.status(400).send({ error: "no hero was provided" });
+    } else {
+      // add a rollbar error here too
+  
+      res.status(400).send({ error: "that hero already exists" });
+    }
   });
 
   const port = process.env.PORT || 4545;
